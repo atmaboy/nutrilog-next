2f858357f2358144507684a04166972f76c0b648
atmaboy <dev.wiryawan@gmail.com>
2026-04-17 23:49:57 +0700

    set sidebar admin show only when user login

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,18 @@
+// app/admin/layout.tsx
+'use client'
+import { usePathname } from 'next/navigation'
 import AdminSidebar from '@/components/admin/AdminSidebar'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  // Auth check dilakukan HANYA oleh middleware.ts
-  // Jangan tambahkan redirect di sini — menyebabkan redirect loop
+  const pathname = usePathname()
+  const isLoginPage = pathname === '/admin/login'
+
   return (
     <div className="flex min-h-screen bg-background">
-      <AdminSidebar />
-      <main className="flex-1 min-w-0 p-6 overflow-auto">{children}</main>
+      {!isLoginPage && <AdminSidebar />}
+      <main className={`${isLoginPage ? 'w-full' : 'flex-1 min-w-0 p-6 overflow-auto'}`}>
+        {children}
+      </main>
     </div>
   )
 }
